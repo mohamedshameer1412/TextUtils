9a03d4df35b70b9f81c5fb6ef75f2aa3b8b05f71
Shameer <mohamedshameer573@gmail.com>
2022-12-21 00:23:14 +0530

    Updates

---
+++ precache-manifest.fc3624fc663eaf698699e6d25e0e9fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc2f440321053f16ff0c",
+    "url": "/TextUtils/static/css/main.7f29de8b.chunk.css"
+  },
+  {
+    "revision": "bc2f440321053f16ff0c",
+    "url": "/TextUtils/static/js/main.bc2f4403.chunk.js"
+  },
+  {
+    "revision": "a3a9185b9e7ea8adaead",
+    "url": "/TextUtils/static/js/1.a3a9185b.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "d1835310618413fe371b",
+    "url": "/TextUtils/static/js/runtime~main.d1835310.js"
+  },
+  {
+    "revision": "cacb3092438398d3944fb3620795ae42",
+    "url": "/TextUtils/index.html"
+  }
+];
